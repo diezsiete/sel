5cad03805ab3ffbaf7dad76ff4bf8f77b35c19bc
Jose Dario Guerrero Aragon <guerrerojosedario@yahoo.com>
2019-05-19 16:17:13 -0500

    Entidad Usuario y Fixtures de Usuario

---
+++ src/DataFixtures/UsuarioFixture.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Usuario;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UsuarioFixture extends BaseFixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+
+    protected function loadData(ObjectManager $manager)
+    {
+        $this->createMany(2, 'main_users', function ($i) use ($manager){
+            $user = new Usuario();
+            $user
+                ->setIdentificacion($i+100)
+                ->setEmail(sprintf('usuario%d@sel.com.co', $i))
+                ->setPrimerNombre($this->faker->firstName)
+                ->setPrimerApellido($this->faker->lastName)
+                ->aceptarTerminos();
+
+            $user->setPassword($this->passwordEncoder->encodePassword($user,'coco'));
+
+            return $user;
+        });
+
+        $this->createMany(2, 'admin_users', function ($i) {
+            $user = new Usuario();
+            $user
+                ->setIdentificacion($i+200)
+                ->setEmail(sprintf('admin%d@sel.com.co', $i))
+                ->setPrimerNombre($this->faker->firstName)
+                ->setPrimerApellido($this->faker->lastName)
+                ->aceptarTerminos();
+
+
+            $user->setPassword($this->passwordEncoder->encodePassword($user,'coco'));
+
+            $user->setRoles(["ROLE_ADMIN"]);
+
+            return $user;
+        });
+
+        $superadmin = new Usuario();
+        $superadmin
+            ->setIdentificacion(1018410666)
+            ->setEmail('guerrerojosedario@gmail.com')
+            ->setPrimerNombre("JOSE")
+            ->setPrimerApellido("GUERRERO")
+            ->setPassword($this->passwordEncoder->encodePassword($superadmin,'coco'))
+            ->setRoles(["ROLE_SUPERADMIN"])
+            ->aceptarTerminos();
+
+        $manager->persist($superadmin);
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Usuario.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UsuarioRepository")
+ */
+class Usuario implements UserInterface
+{
+    use TimestampableEntity;
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $identificacion;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=140, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $primerNombre;
+
+    /**
+     * @ORM\Column(type="string", length=60, nullable=true)
+     */
+    private $segundoNombre;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $primerApellido;
+
+    /**
+     * @ORM\Column(type="string", length=60, nullable=true)
+     */
+    private $segundoApellido;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $activo = true;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $aceptoTerminosEn;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $ultimoLogin;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdentificacion(): ?string
+    {
+        return $this->identificacion;
+    }
+
+    public function setIdentificacion(string $identificacion): self
+    {
+        $this->identificacion = $identificacion;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->identificacion;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPrimerNombre(): ?string
+    {
+        return $this->primerNombre;
+    }
+
+    public function setPrimerNombre(string $primerNombre): self
+    {
+        $this->primerNombre = $primerNombre;
+
+        return $this;
+    }
+
+    public function getSegundoNombre(): ?string
+    {
+        return $this->segundoNombre;
+    }
+
+    public function setSegundoNombre(?string $segundoNombre): self
+    {
+        $this->segundoNombre = $segundoNombre;
+
+        return $this;
+    }
+
+    public function getPrimerApellido(): ?string
+    {
+        return $this->primerApellido;
+    }
+
+    public function setPrimerApellido(string $primerApellido): self
+    {
+        $this->primerApellido = $primerApellido;
+
+        return $this;
+    }
+
+    public function getSegundoApellido(): ?string
+    {
+        return $this->segundoApellido;
+    }
+
+    public function setSegundoApellido(?string $segundoApellido): self
+    {
+        $this->segundoApellido = $segundoApellido;
+
+        return $this;
+    }
+
+    public function getActivo(): ?bool
+    {
+        return $this->activo;
+    }
+
+    public function setActivo(bool $activo): self
+    {
+        $this->activo = $activo;
+
+        return $this;
+    }
+
+    public function getAceptoTerminosEn(): ?\DateTimeInterface
+    {
+        return $this->aceptoTerminosEn;
+    }
+
+    public function aceptarTerminos(): self
+    {
+        $this->aceptoTerminosEn = new \DateTime();
+
+        return $this;
+    }
+
+    public function getUltimoLogin(): ?\DateTimeInterface
+    {
+        return $this->ultimoLogin;
+    }
+
+    public function setUltimoLogin(?\DateTimeInterface $ultimoLogin): self
+    {
+        $this->ultimoLogin = $ultimoLogin;
+
+        return $this;
+    }
+}
