17ac0586cbe27166ad4f9435334411a12ad5c8bd
Jose Dario Guerrero Aragon <guerrerojosedario@yahoo.com>
2019-10-31 14:01:58 -0500

    webpack instalado y funcionando en pta incluyendo solo jquery

--- config/bundles.php
+++ config/bundles.php
@@ -23,4 +23,5 @@ return [
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
