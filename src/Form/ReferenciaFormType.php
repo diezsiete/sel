d8c5bc84ee6b7afa8c6ac2868cd70717c52da7dd
Jose Dario Guerrero Aragon <guerrerojosedario@yahoo.com>
2019-12-05 02:46:29 -0500

    fix servilabor hv referencia

--- src/Form/ReferenciaFormType.php
+++ src/Form/ReferenciaFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Constant\HvConstant;
 use App\Entity\Referencia;
+use App\Service\Configuracion\Configuracion;
+use League\Flysystem\Config;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -11,12 +13,31 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ReferenciaFormType extends AbstractType
 {
+    /**
+     * @var Configuracion
+     */
+    private $configuracion;
+
+    public function __construct(Configuracion $configuracion)
+    {
+        $this->configuracion = $configuracion;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $tiposReferencia = HvConstant::REFERENCIA_TIPO;
+        //fix para servilabor en tipo de referencia que tiene id diferente
+        if($this->configuracion->getEmpresa(true) === 'servilabor') {
+            $tiposReferencia = array_map(function ($id) {
+                return $id + 6;
+            }, $tiposReferencia);
+        }
+        $tiposReferencia = array_flip($tiposReferencia);
+
         $builder
             ->add('tipo', ChoiceType::class, [
                 'label' => 'Tipo de referencia',
-                'choices' => array_flip(HvConstant::REFERENCIA_TIPO),
+                'choices' => $tiposReferencia,
                 'placeholder' => 'Seleccione...'
             ])
             ->add('nombre', null, [
